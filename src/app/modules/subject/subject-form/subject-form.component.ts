65813f787e6235fe05900dd42c6affcb9a55b0c4
Mikael Araya <Mikaelaraya@example.com>
2018-06-08 01:45:57 +0300

    fixed subject form initializer

--- src/app/modules/subject/subject-form/subject-form.component.ts
+++ src/app/modules/subject/subject-form/subject-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SubjectService, ISubject } from '../subject.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -12,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class SubjectFormComponent implements OnInit {
     subject: ISubject;
     subjectForm: FormGroup;
+    subjectArray: FormArray;
     id: number;
     isUpdate: boolean;
   SUBJECT_TYPE = ['Elective', 'Major', 'General'];
@@ -20,38 +21,72 @@ export class SubjectFormComponent implements OnInit {
               private formBuilder: FormBuilder,
               private activatedRoute: ActivatedRoute
             ) {
-              this.generateForm();
+              this.subjectForm = this.formBuilder.group({
+                subjectArray: this.formBuilder.array([])
+              });
             }
-
+            initSubject(subject: any = '') {
+              // initialize our address
+              return this.formBuilder.group({
+                  title: [(subject) ? subject.title : '', Validators.required],
+                  creditHr: [(subject) ? subject.title : '', Validators.required ],
+                  type: [(subject) ? subject.type : '', Validators.required ]
+              });
+          }
       ngOnInit() {
         this.id = +this.activatedRoute.snapshot.paramMap.get('id');
         if (this.id) {
           this.isUpdate = true;
           this.subjectService.getSubjects(this.id).subscribe((response: any) => this.generateForm(response.result));
         } else {
+          this.subjectForm = this.formBuilder.group({
+            subjectArray: this.formBuilder.array([this.initSubject()])
+          });
+
           this.isUpdate = false;
         }
       }
+      addSubject() {
+        const add = <FormArray>this.subjectForm.controls['subjectArray'];
+        add.push(this.initSubject());
+      }
+      removeSubject(i: number) {
+        // remove address from the list
+        const control = <FormArray>this.subjectForm.controls.subjectArray;
+        control.removeAt(i);
+    }
 
-      private generateForm(currentSubject: any = '') {
+    get subjects() { return (<FormArray>this.subjectForm.controls.subjectArray).controls; }
+
+    private generateForm(currentSubject: any[] = []) {
         const subject = currentSubject;
-        console.log(currentSubject);
-        this.subjectForm = this.formBuilder.group({
+        console.log(currentSubject.length);
+
+        currentSubject.forEach((sub) => {
+                (<FormArray>this.subjectForm.controls['subjectArray']).controls
+                .push(this.initSubject(sub));
+        });
+
+    /*this.subjectForm = this.formBuilder.group({
           title : this.buildControl(subject.title, true),
           type : this.buildControl(subject.subject_type, true),
           creditHr : this.buildControl(subject.grade_weightage, true)
         });
-
+*/
       }
 
       private prepareData(): any {
-        const formModel = this.subjectForm.value;
-        const subjectData = {
-          title: formModel.title ? formModel.title : '',
-          type:  formModel.type ? formModel.type : '',
-          creditHr: formModel.creditHr ? formModel.creditHr : ''
+        const formModel = <FormArray>this.subjectForm.controls.subjectArray;
+        const subjectData = [];
+        formModel.controls.forEach((subject, i) => {
+        subjectData [i] = {
+          title: subject.value.title ? subject.value.title : '',
+          type:  subject.value.type ? subject.value.type : '',
+          creditHr: subject.value.creditHr ? subject.value.creditHr : ''
 
         };
+      });
+      console.log(subjectData);
         return subjectData;
       }
 
