513a205126ca19563dea2857c43529525e362d19
Mikael Araya <Mikaelaraya@example.com>
2018-06-08 21:52:49 +0300

    completed subject module
    this module can add 1 or many subjects at a time.
    currently the module only updates one subject at a time

--- src/app/modules/subject/subject-form/subject-form.component.ts
+++ src/app/modules/subject/subject-form/subject-form.component.ts
@@ -10,105 +10,96 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class SubjectFormComponent implements OnInit {
-  subject: ISubject;
-  subjectForm: FormGroup;
-  subjectArray: FormArray;
-  id: number;
-  isUpdate: boolean;
-  SUBJECT_TYPE = ['Elective', 'Major', 'General'];
-  constructor(
-    private subjectService: SubjectService,
-    private formBuilder: FormBuilder,
-    private activatedRoute: ActivatedRoute
-  ) {
-    this.subjectForm = this.formBuilder.group({
-      subjectArray: this.formBuilder.array([])
-    });
-  }
-  initSubject(subject: any = '') {
-    // initialize our subject
-    return this.formBuilder.group({
-      title: [(subject) ? subject.title : '', Validators.required],
-      creditHr: [(subject) ? subject.title : '', Validators.required],
-      type: [(subject) ? subject.type : '', Validators.required]
-    });
-  }
-  ngOnInit() {
-    this.id = +this.activatedRoute.snapshot.paramMap.get('id');
-    if (this.id) {
-      this.isUpdate = true;
-      this.subjectService.getSubjects(this.id).subscribe((response: any) => this.generateForm(response.result));
-    } else {
-      this.subjectForm = this.formBuilder.group({
-        subjectArray: this.formBuilder.array([this.initSubject()])
-      });
+      private subject: ISubject;
+              subjectForm: FormGroup;
+              subjectArray: FormArray;
+              id: number;
+              isUpdate: boolean;
+              SUBJECT_TYPE = ['Elective', 'Major', 'General'];
 
-      this.isUpdate = false;
-    }
-  }
-  addSubject() {
-    const add = <FormArray>this.subjectForm.controls['subjectArray'];
-    add.push(this.initSubject());
-  }
+       constructor(
+                    private subjectService: SubjectService,
+                    private formBuilder: FormBuilder,
+                    private activatedRoute: ActivatedRoute
+                  ) {
+            this.subjectForm = this.formBuilder.group({
+              subjectArray: this.formBuilder.array([])
+            });
+        }
+      ngOnInit() {
+          this.id = +this.activatedRoute.snapshot.paramMap.get('id');
+          if (this.id) {
+            this.isUpdate = true;
+            this.subjectService.getSubjects(this.id).subscribe((response: any) => this.generateForm(response.result));
+          } else {
+            this.subjectForm = this.formBuilder.group({
+              subjectArray: this.formBuilder.array([this.initSubject()])
+            });
 
-  removeSubject(i: number) {
-    // remove subject from the list
-    const control = <FormArray>this.subjectForm.controls.subjectArray;
-    control.removeAt(i);
-  }
+            this.isUpdate = false;
+          }
+      }
 
-  get subjects() { return (<FormArray>this.subjectForm.controls.subjectArray).controls; }
+      get subjects() { return (<FormArray>this.subjectForm.controls.subjectArray).controls; }
 
-  private generateForm(currentSubject: any[] = []) {
-    const subject = currentSubject;
-    console.log(currentSubject.length);
+      private  initSubject(subject: any = '') {
+          // initialize our subject
+          return this.formBuilder.group({
+            title: [(subject) ? subject.title : '', Validators.required],
+            creditHr: [(subject) ? subject.title : '', Validators.required],
+            type: [(subject) ? subject.type : '', Validators.required]
+          });
+      }
 
-    currentSubject.forEach((sub) => {
-      (<FormArray>this.subjectForm.controls['subjectArray']).controls
-        .push(this.initSubject(sub));
-    });
+      addSubject() {
+        const add = <FormArray>this.subjectForm.controls['subjectArray'];
+        add.push(this.initSubject());
+      }
 
-  }
+      removeSubject(i: number) {// remove subject from the list
+        const control = <FormArray>this.subjectForm.controls.subjectArray;
+        control.removeAt(i);
+      }
 
-  private prepareData(): any {
-    const formModel = <FormArray>this.subjectForm.controls.subjectArray;
-    const subjectData = [];
-    formModel.controls.forEach((subject, i) => {
-      subjectData[i] = {
-        title: subject.value.title ? subject.value.title : '',
-        type: subject.value.type ? subject.value.type : '',
-        creditHr: subject.value.creditHr ? subject.value.creditHr : ''
+      private generateForm(currentSubject: any[] = []) {
+          currentSubject.forEach((sub) => {
+            (<FormArray>this.subjectForm.controls['subjectArray']).controls
+              .push(this.initSubject(sub));
+          });
+      }
 
-      };
-    });
-    console.log(subjectData);
-    return subjectData;
-  }
+      private prepareData(): any {
+        const formModel = <FormArray>this.subjectForm.controls.subjectArray;
+        const subjectData = [];
 
-  private buildControl(value: any = '', required: boolean = false) {
-    return (required) ? [value, Validators.required] : value;
-  }
+        formModel.controls.forEach((subject, i) => {
+            subjectData[i] = {
+              title: subject.value.title ? subject.value.title : '',
+              type: subject.value.type ? subject.value.type : '',
+              creditHr: subject.value.creditHr ? subject.value.creditHr : ''
+            };
+          });
+        return subjectData;
+      }
 
-  onSubmit() {
-    this.subject = this.prepareData();
-    if (this.isUpdate) {
-      this.subjectService.updateSubject(this.subject, this.id)
-        .subscribe((response: any) => this.handelResponse(response));
-    } else {
-      this.subjectService.addSubject(this.subject)
-        .subscribe((response: any) => this.handelResponse(response));
-    }
-  }
+      onSubmit() {
+        this.subject = this.prepareData();
+        if (this.isUpdate) {
+          this.subjectService.updateSubject(this.subject, this.id)
+            .subscribe((response: any) => this.handelResponse(response));
+        } else {
+          this.subjectService.addSubject(this.subject)
+            .subscribe((response: any) => this.handelResponse(response));
+        }
+      }
 
-  private handelResponse(response: any) {
-    if (response.success) {
-      alert('success');
-    } else {
-      alert('failed');
-    }
-    console.log(response);
-  }
-
-
+      private handelResponse(response: any) {
+        if (response.success) {
+          alert('success');
+        } else {
+          alert('failed');
+        }
+        console.log(response);
+      }
 
 }
